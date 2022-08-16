a441944325ea9c2c65a0f674a78f0dee5849d6fa
andressaaborges <emylleaborges@gmail.com>
2022-08-15 22:01:58 -0300

    feat: add data validation function

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 let salaryFinal = 0;
-let salaryInitial = document.getElementById('salary');
-let workedDays = document.getElementById('days');
-let resultSalaryFinal = document.getElementById('result_salary_final');
+let salaryInitial = document.getElementById("salary");
+let workedDays = document.getElementById("days");
+let resultSalaryFinal = document.getElementById("result_salary_final");
 const hourDaily = 8;
 
 const calculateSalaryFinal = () => {
-    salaryFinal = (salaryInitial.value * hourDaily) * workedDays.value;
-    resultSalaryFinal.innerHTML = salaryFinal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-}
+    if (!isEmpty()) {
+        salaryFinal = salaryInitial.value * hourDaily * workedDays.value;
+        writeSalaryFinal(salaryFinal);
+    }
+};
 
-const calculateSalary = () => {
-    salaryFinal = (salaryInitial.value / workedDays.value) / hourDaily;
-    resultSalaryFinal.innerHTML = salaryFinal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-}
+const calculateSalaryHour = () => {
+    if (!isEmpty()) {
+        salaryFinal = salaryInitial.value / workedDays.value / hourDaily;
+        writeSalaryFinal(salaryFinal);
+    }
+};
+
+const writeSalaryFinal = (valueSalaryFinal) => {
+    resultSalaryFinal.innerHTML = valueSalaryFinal.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+  });
+};
+
+const isEmpty = () => {
+    if (salaryInitial.value == "" || workedDays.value == "") {
+        alert("Digite um valor númerico e não deixe campos vazios!");
+        return true;
+    }
+    return false;
+};
